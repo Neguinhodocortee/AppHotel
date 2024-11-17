ac88fe88d1f07057355358e6edb353a744cd5d0b
Neusa <Neusa@LAPTOP-7UABLNGO>
2024-11-17 02:09:49 -0300

    Adicionar arquivos de projeto.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace AppHotel
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        // Evento para o botão "Sobre"
+        private async void OnSobreButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SobrePage());
+        }
+    }
+}
